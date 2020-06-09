419bae8ec7bb2360a7e2f77dca8661f8bdda264c
Mason Proffitt <mproffit@cern.ch>
2020-06-09 12:44:16 +0200

    implement continued fraction estimation

--- qfactor/shors_quantum.py
+++ qfactor/shors_quantum.py
@@ -1,2 +1,63 @@
+import math
+
+
 def get_order(x, n):
-    raise NotImplementedError('Order-finding algorithm not yet implemented')
+    q = get_q(n)
+
+    while True:
+        c = get_c(x, n, q)
+        r_candidate = find_nearest_fraction(c / q, n - 1)[1]
+        if x ** r_candidate % n == 1:
+            return r_candidate
+
+
+def get_q(n):
+    return 2 ** math.ceil(2 * math.log2(n))
+
+
+def get_c(x, n, q):
+    raise NotImplementedError('Quantum part of algorithm not yet implemented')
+
+
+def find_nearest_fraction(original_number, max_denominator):
+    if original_number < 0:
+        raise ValueError('Original number must be greater than or equal to 0;'
+                         + f' found {original_number} instead')
+
+    if max_denominator < 1:
+        raise ValueError('Max denominator must be greater than or equal to 1;'
+                         + f' found {max_denominator} instead')
+
+    expansion = []
+    current_number = original_number
+    while True:
+        integer_part = math.floor(current_number)
+        fractional_part = current_number % 1
+        expansion.append(integer_part)
+        if fractional_part == 0 or get_fraction(expansion)[1] >= max_denominator:
+            break
+        else:
+            current_number = fractional_part ** -1
+
+    if get_fraction(expansion)[1] > max_denominator:
+        fraction_to_return = list(get_fraction(expansion[:-2]))
+        second_to_last_fraction = get_fraction(expansion[:-1])
+        while fraction_to_return[1] + second_to_last_fraction[1] <= max_denominator:
+            fraction_to_return[0] += second_to_last_fraction[0]
+            fraction_to_return[1] += second_to_last_fraction[1]
+        return tuple(fraction_to_return)
+    else:
+        return get_fraction(expansion)
+
+
+def get_fraction(expansion):
+    if len(expansion) == 0:
+        raise IndexError('Continued fraction expansion cannot be empty')
+    elif len(expansion) == 1:
+        if expansion[0] % 1 != 0:
+            raise ValueError('Continued fraction expansion must be composed of integers;'
+                             + f' found {expansion[0]} instead')
+        return (expansion[0], 1)
+    else:
+        nested_fraction = get_fraction(expansion[1:])
+        return (expansion[0] * nested_fraction[0] + nested_fraction[1], nested_fraction[0])
---
+++ test/test_shors_quantum.py
@@ -0,0 +1,77 @@
+import pytest
+
+from qfactor.shors_quantum import get_order, get_q, find_nearest_fraction, get_fraction
+
+
+def test_get_order_bad_type():
+    with pytest.raises(TypeError):
+        get_order('asdf', 15)
+        get_order(2, 'asdf')
+
+
+def test_get_order_bad_value():
+    with pytest.raises(ValueError):
+        get_order(-1, 15)
+    with pytest.raises(ValueError):
+        get_order(0, 15)
+    with pytest.raises(ValueError):
+        get_order(0.5, 15)
+
+
+def test_get_order_good():
+    assert get_order(2, 3) == 2
+    assert get_order(7, 15) == 4
+
+
+def test_get_q_bad_type():
+    with pytest.raises(TypeError):
+        get_q('asdf')
+
+
+def test_get_q_good():
+    assert get_q(3) == 16
+    assert get_q(15) == 256
+
+
+def test_find_nearest_fraction_bad_type():
+    with pytest.raises(TypeError):
+        find_nearest_fraction('asdf', 1)
+    with pytest.raises(TypeError):
+        find_nearest_fraction(0, 'asdf')
+
+
+def test_find_nearest_fraction_bad_value():
+    with pytest.raises(ValueError):
+        find_nearest_fraction(-1, 1)
+    with pytest.raises(ValueError):
+        find_nearest_fraction(0, -1)
+    with pytest.raises(ValueError):
+        find_nearest_fraction(0, 0)
+    with pytest.raises(ValueError):
+        find_nearest_fraction(0, 0.5)
+
+
+def test_find_nearest_fraction():
+    assert find_nearest_fraction(0.4, 2) == (1, 2)
+    assert find_nearest_fraction(0.6, 3) == (2, 3)
+
+
+def test_get_fraction_bad_size():
+    with pytest.raises(IndexError):
+        get_fraction([])
+
+
+def test_get_fraction_bad_type():
+    with pytest.raises(TypeError):
+        get_fraction(['asdf'])
+
+
+def test_get_fraction_bad_value():
+    with pytest.raises(ValueError):
+        get_fraction([0.5])
+
+
+def test_get_fraction_good():
+    assert get_fraction([2]) == (2, 1)
+    assert get_fraction([0, 2]) == (1, 2)
+    assert get_fraction([0, 2, 3]) == (3, 7)
